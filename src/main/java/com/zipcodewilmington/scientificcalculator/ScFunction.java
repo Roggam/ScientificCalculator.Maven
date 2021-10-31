c5c7497b03d1c3b04eb563e9af30ccc89c1c52fb
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-10-31 11:11:43 -0400

    commit changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/ScFunction.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScFunction.java
@@ -1,102 +1,8 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class ScFunction {
+
     private Double result;
-
-<<<<<<< HEAD
-    public ScFunction() {
-        result = 0.0; // constructor to initialize variable
-    }
-
-//    public switchDisplayMode() {
-//
-//    }
-//
-//    public switchDisplayMode(String mode) {
-//
-//    }
-
-
-    public double sin(double num1) {
-        result = Math.sin(num1);
-        return result;
-        // this works
-    }
-
-    public double cosine(double num1) {
-        result = Math.cos(num1);
-        return result;
-        // this works
-    }
-
-
-    public double tangent(double num1) {
-        result = Math.tan(num1);
-        return result;
-        // this works
-    }
-
-
-
-    public double inverseSin(double num1) {
-        result = Math.asin(num1);
-        return result;
-        // this works
-        // num1 NEEDS TO BE BETWEEN -1 AND 1 otherwise NaN.
-    }
-
-
-
-    public double inverseCosine(double num1) {
-        result = Math.acos(num1);
-        return result;
-        // this works
-        // num1 NEEDS TO BE BETWEEN -1 AND 1 otherwise NaN
-    }
-
-
-    public double inverseTangent(double num1) {
-        result = Math.atan(num1);
-        return result;
-        // this works
-        // no input restrictions
-    }
-
-
-    public double log(double num1, double num2) {
-        result = Math.log(num1);
-        return result;
-        // need to revisit--not sure
-    }
-
-
-    public double inverseLog(double num1) {
-        result = Math.exp(Math.log(num1));
-        return result;
-        // inputs are traditionally known as x and y
-        // needs two inputs
-    }
-
-
-//    public double naturalLog(double num1) {
-//
-//    }
-//
-//    public double inverseNaturalLog(double num1) {
-//
-//    }
-//
-    public  Double factorial(double num1) {
-        double i;
-        while (i <= num1) {
-            result = result * i;
-            i++;
-        }
-//        return result;
-    }
-=======
-    private Double result;
->>>>>>> e8c1d0945ccf2f31beb19dc54ed0acae8933f15f
 
     // 1 for degree & 2 for radian
     private int unitsMode;
@@ -234,5 +140,3 @@ public class ScFunction {
         }
     }
 }
-
-
